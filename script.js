991b49c8584fcbed0e86d0deff804053d6200d7a
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-14 12:30:54 -0600

    Adjusting scores based on a correct guess or a wrong guess

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
 console.log(secretNumber);
 
@@ -12,9 +13,15 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number!';
   } else if (Number(guess) === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    score++;
+    document.querySelector('.score').textContent = score;
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Guess is Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Guess is Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
