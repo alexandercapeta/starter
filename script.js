5815bc7b05c5262264dd25374b9270c265d9a0f2
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-14 11:31:57 -0600

    Adding the random number generator

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const number = Math.trunc(Math.random()) + 1;
+
 document.querySelector('check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
   console.log(guess);
