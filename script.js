9217bbc67309ddec1577fc8045e9804956184ce5
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-14 11:46:55 -0600

    Correcting the name of a class in the query selector to '.check'

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const secretNumber = Math.trunc(Math.random()) + 1;
 
-document.querySelector('check').addEventListener('click', function () {
+document.querySelector('.check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
   console.log(guess);
 
