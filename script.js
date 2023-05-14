ffd16a41a7e1df945e7ff2f0b230c0abe9e39a7a
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-14 11:37:18 -0600

    Changing the 'number' variable to 'secretNumber'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const number = Math.trunc(Math.random()) + 1;
+const secretNumber = Math.trunc(Math.random()) + 1;
 
 document.querySelector('check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
