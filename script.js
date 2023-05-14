1bd8a93ea2d7351a0bc140ffa8cbe0090a090440
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-14 11:41:40 -0600

    Adding the logic to determine if the guess is the correct number.

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ const secretNumber = Math.trunc(Math.random()) + 1;
 document.querySelector('check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
   console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  }
 });
