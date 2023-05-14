d14860db9b8118db01ea811b2aafa72e377aecd0
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-14 13:24:32 -0600

    Removing the logical step for checking the score when the guess is right.

--- script.js
+++ script.js
@@ -13,14 +13,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number!';
   } else if (Number(guess) === secretNumber) {
     // When the player wins.
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Correct Number!';
-      score++;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
+
+    document.querySelector('.message').textContent = 'Correct Number!';
+    score++;
+    document.querySelector('.score').textContent = score;
   } else if (guess > secretNumber) {
     // When the guess is too high.
     if (score > 1) {
