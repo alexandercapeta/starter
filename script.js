a4e7296968b7105b320a78830975d353f3cd5f86
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-18 20:40:06 -0600

    Creating a function to display the message without repeating code.

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
